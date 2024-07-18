d2b2fb4e1ed45ef8c316334d86a60b984f9c5d85
aditya mehto <adityamehto19@gmail.com>
2024-07-18 12:29:50 +0530

    5th commit

--- Building A Ecommerce website sharpner/vite-project/src/App.jsx
+++ Building A Ecommerce website sharpner/vite-project/src/App.jsx
@@ -12,12 +12,23 @@ const App = () => {
   return (
     <GlobalContextProvider>
       <NavbarComponent />
+      <div style={{ backgroundColor: "gray", height: "190px" }}>
+        <h1 style={{ textAlign: "center", color: "white", fontSize: "100px" }}>
+          The Generics
+        </h1>
+
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/about" element={<About />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
+      <div style={{background:"#56CCF2", height:"150px"}}>
+         <h1 style={{ textAlign: "center", color: "white", fontSize: "50px", paddingRight:"800px", paddingTop:"20px"}}>
+          The Generics
+         </h1>
+      </div>
     </GlobalContextProvider>
   );
 };
--- Building A Ecommerce website sharpner/vite-project/src/components/Home.jsx
+++ Building A Ecommerce website sharpner/vite-project/src/components/Home.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
-import NavbarComponent from './NavbarComponent'
+import React from "react";
+import Table from "react-bootstrap/esm/Table";
 
 const Home = () => {
   return (
     <div>
-    <h2>Home</h2>
-
+      <h2 style={{ textAlign: "center", paddingTop: "20px" }}>TOURS</h2>
+      <Table responsive style={{ width: "60%", margin: "auto"}}>
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>JUL16</td>
+            <td>DETROIT, MI</td>
+            <td>DTE ENERGY MUSIC THEATRE</td>
+            <td>
+              <button style={{background:"#56CCF2", border:"none", borderRadius:"5px"}}>Buy TICKETS</button>
+            </td>
+          </tr>
+          <tr>
+            <td>JUL19</td>
+            <td>TORONTO,ON</td>
+            <td>BUDWEISER STAGE</td>
+            <td>
+            <button style={{background:"#56CCF2", border:"none", borderRadius:"5px"}}>Buy TICKETS</button>
+            </td>
+          </tr>
+          <tr>
+            <td>JUL 22</td>
+            <td>BRISTOW, VA</td>
+            <td>JIGGY LUBE LIVE</td>
+            <td>
+            <button style={{background:"#56CCF2", border:"none", borderRadius:"5px"}}>Buy TICKETS</button>
+            </td>
+          </tr>
+          <tr>
+            <td>JUL 29</td>
+            <td>PHOENIX, AZ</td>
+            <td>AK-CHIN PAVILION</td>
+            <td>
+            <button style={{background:"#56CCF2", border:"none", borderRadius:"5px"}}>Buy TICKETS</button>
+            </td>
+          </tr>
+          <tr>
+            <td>AUG 2</td>
+            <td>LAS VEGAS, NV</td>
+            <td>T-MOBILE ARENA</td>
+            <td>
+            <button style={{background:"#56CCF2", border:"none", borderRadius:"5px"}}>Buy TICKETS</button>
+            </td>
+          </tr>
+          <tr>
+            <td>AUG 7</td>
+            <td>CONCORD, CA</td>
+            <td>CONCORD PAVILION</td>
+            <td>
+            <button style={{background:"#56CCF2", border:"none", borderRadius:"5px"}}>Buy TICKETS</button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
